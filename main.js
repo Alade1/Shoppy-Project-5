b80cf28da08b9a4c505ffc9991dcf54fd4805195
Alade1 <aladefolarin7@gmail.com>
2023-08-26 13:17:04 +0100

    fully responsive site

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const dropDownButton = document.querySelector(".dropdown-button");
+    const dropDownMenu = document.querySelector(".dropdown-menu");
+
+ dropDownButton.addEventListener("click", function(){
+    dropDownMenu.style.display = dropDownMenu.style.display === "block" ? "none" : "block";
+
+ })
+})
